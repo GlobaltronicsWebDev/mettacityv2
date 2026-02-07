55674d69a3a744fff2ccec1145ffb9030bf9020c
Globaltronics <akirosefederez99@gmail.com>
2026-02-08 00:13:12 +0800

    index.php

--- public/index.php
+++ public/index.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
@@ -6,15 +9,15 @@ use Illuminate\Http\Request;
 define('LARAVEL_START', microtime(true));
 
 // Determine if the application is in maintenance mode...
-if (file_exists($maintenance = __DIR__.'/../mettacity/storage/framework/maintenance.php')) {
-    require $maintenance;
+if (file_exists(__DIR__.'/laravel/storage/framework/maintenance.php')) {
+require __DIR__.'/laravel/storage/framework/maintenance.php';
 }
 
 // Register the Composer autoloader...
-require __DIR__.'/../mettacity/vendor/autoload.php';
+require __DIR__.'/laravel/vendor/autoload.php';
 
 // Bootstrap Laravel and handle the request...
 /** @var Application $app */
-$app = require_once __DIR__.'/../mettacity/bootstrap/app.php';
+$app = require __DIR__.'/laravel/bootstrap/app.php';
 
 $app->handleRequest(Request::capture());
